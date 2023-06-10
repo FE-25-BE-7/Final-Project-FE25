1127bf26cc113db2cee06c4bdfc5b267341e3b20
Handayani Meiatara <hmeiatara@gmail.com>
2023-06-10 21:45:58 +0700

    deleting some files

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import NavBar from "./components/Navbar"
 import Footer from "./components/Footer";
 import { BrowserRouter as Router,  Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
-import { Article } from "./pages/Article";
-import { Program } from "./pages/Program";
 import { About } from "./pages/About";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Register";
-import { Donasi } from "./pages/Donasi";
-
 
 
 function App() {
@@ -22,12 +16,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/artikel" element={<Article />} />
-            <Route path="/program" element={<Program />} />
             <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/donasi" element={<Donasi />} />
           </Routes>
         </div>
 
