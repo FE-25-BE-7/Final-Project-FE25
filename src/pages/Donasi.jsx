aead5243ba7cdb7ea5bbd0939c5f180dd844617b
Panggih Dwi Alamsyah <pdwialamsyah@gmail.com>
2023-06-11 21:59:25 +0700

    Fix Donasi

--- src/pages/Donasi.jsx
+++ src/pages/Donasi.jsx
@@ -1,132 +1,136 @@
-import React from "react";
-import {  FaCreditCard} from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { FaCreditCard, FaPhoneAlt } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-import "./pages.css";
+import axios from "axios";
+import "./Donasi.css";
 
 export const Donasi = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [jml_donasi, setJml_donasi] = useState("");
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const response = await axios.get(
+          "https://64512297a3221969115bd221.mockapi.io/user/token"
+        );
+        setToken(response.data.token);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchToken();
+  }, []);
+
+  const handleDonasi = async (e) => {
+    e.preventDefault();
+
+    const userData = {
+      username: username,
+      email: email,
+      phone: phone,
+      jml_donasi: jml_donasi,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://64512297a3221969115bd221.mockapi.io/user?token=" + token,
+        userData
+      );
+
+      // Logika untuk menangani respon dari API setelah pengiriman data pengguna
+      console.log(response.data);
+
+      // Reset form setelah pengiriman data pengguna berhasil
+      setUsername("");
+      setEmail("");
+      setPhone("");
+      setJml_donasi("");
+    } catch (error) {
+      // Logika untuk menangani kesalahan jika terjadi error saat pengiriman data pengguna
+      console.log(error);
+    }
+  };
+
   return (
-    <>
-    <body>
+    <> 
+    <body> 
     <section className="section form-con" id="login">
-        <div className= "container form-box wrapper">
+      <div className="container form-box wrapper">
         <h2>Form Donasi</h2>
-        <form method="POST">
-            <h4>AKUN</h4>
-
-            <div className="input-group">
-                <div className="input-box">
-                    <input type="text" placeholder="Username" required className="name" />
-                    <i className="fa fa-user icon"></i>
-                </div>
-            </div>
-            <div className="input-group">
-                <div className="input-box">
-                    <input type="email" placeholder="Masukan Email" required className="name" />
-                    <i className="fa fa-envelope icon"></i>
-                </div>
-            </div>
-
-            {/* <div className="input-group">
-                <div className="input-box">
-                    <h4> Tanggal Lahir</h4>
-                    <input type="text" placeholder="DD" className="dob" />
-                    <input type="text" placeholder="MM" className="dob" />
-                    <input type="text" placeholder="YYYY" className="dob" />
-                </div>
-                <div className="input-box">
-                    <h4> Jenis Kelamin</h4>
-                    <input type="radio" id="b1" name="gendar" checked className="radio" />
-                    <label for="b1">Pria</label>
-                    <input type="radio" id="b2" name="gendar" className="radio" />
-                    <label for="b2">Wanita</label>
-                </div>
-            </div> */}
-
-
-            <div className="input-group">
-                <div className="input-box">
-                    <h4>Details Donasi</h4>
-                    <input type="radio" name="pay" id="bc1" checked className="radio" />
-                    <label for="bc1"><span> <FaCreditCard> </FaCreditCard> Credit Card</span></label>
-                    <input type="radio" name="pay" id="bc2" className="radio" />
-                    <label for="bc2"><span><i className="fab fa-cc-paypal"></i> Paypal</span></label>
-                </div>
-            </div>
-            
-            <div className="input-group">
-                <div className="input-box">
-                    <input type="tel" placeholder="Nomer Kartu" required className="name" />
-                    <i className="fa fa-credit-card icon"></i>
-                </div>
-            </div>
-
-            {/* <div className="input-group">
-                <div className="input-box">
-                    <input type="tel" placeholder="Card CVC" required className="name" />
-                    <i className="fa fa-user icon"></i>
-                </div>
-                <div className="input-box">
-                    <select>
-                        <option>01 jun</option>
-                        <option>02 jun</option>
-                        <option>03 jun</option>
-                    </select>
-                    <select>
-                        <option>2020</option>
-                        <option>2021</option>
-                        <option>2022</option>
-                    </select>
-                </div>
-            </div>  */}
-            
-            <div className=" input-group">
-              <div className=" input-box">
-              <div>
-                <input type="radio" name="pay" id="bc3" checked className="radio" />
-                <label for="bc3"><span> <i className="fa-solid fa-rupiah-sign"></i>5.000</span></label>
-
-                <input type="radio" name="pay" id="bc4" className="radio" />
-                <label for="bc4"><span><i className="fa-solid fa-rupiah-sign"></i> 10.000</span></label>
-
-                <input type="radio" name="pay" id="bc5" checked className="radio" />
-                <label for="bc5"><span> <i className="fa-solid fa-rupiah-sign"></i> 20.000</span></label>
-
-                <input type="radio" name="pay" id="bc6" className="radio" />
-                <label for="bc6"><span><i className="fa-solid fa-rupiah-sign"></i> 30.000</span></label>
-
-                <input type="radio" name="pay" id="bc7" checked className="radio" />
-                <label for="bc7"><span> <i className="fa-solid fa-rupiah-sign"></i> 50.000</span></label>
-
-                <input type="radio" name="pay" id="bc8" className="radio" />
-                <label for="bc8"><span><i className="fa-solid fa-rupiah-sign"></i> 100.000</span></label>
-
-                {/* <button className="nominal-button" onclick="selectNominal(100000)">100.000</button>
-                <button className="nominal-button" onclick="selectNominal(200000)">200.000</button>
-                <button className="nominal-button" onclick="selectNominal(300000)">300.000</button> */}
-
-                 </div>
+        <form onSubmit={handleDonasi}>
+          <h4>AKUN</h4>
 
               <div className="input-group">
-                  <div className="input-box">
-                  <input type="text" placeholder="Jumlah Lain" required className="name" />
-                  <i className="fas fa-donate icon "></i>
-                 </div>
-            </div>
-              </div>
-            </div>
-
-            <div className="input-group">
                 <div className="input-box">
-                    <button type="submit">Donasi Sekarang</button>
+                  <input
+                    type="text"
+                    placeholder="Username"
+                    required
+                    className="name"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                  <i className="fa fa-user icon"></i>
                 </div>
+              </div>
 
-            </div>
+              <div className="input-group">
+                <div className="input-box">
+                  <input
+                    type="email"
+                    placeholder="Masukan Email"
+                    required
+                    className="name"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <i className="fa fa-envelope icon"></i>
+                </div>
+              </div>
+
+              <div className="input-group">
+                <div className="input-box">
+                  <input
+                    type="tel"
+                    placeholder="Nomer Telepon"
+                    required
+                    className="name"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                  <i className="fa fa-phone icon"></i>
+                </div>
+              </div>
+
+              <div className="input-group">
+                <div className="input-box">
+                  <input
+                    type="text"
+                    placeholder="Jumlah Donasi"
+                    required
+                    className="name"
+                    value={jml_donasi}
+                    onChange={(e) => setJml_donasi(e.target.value)}
+                  />
+                  <i className="fas fa-donate icon"></i>
+                </div>
+              </div>
+
+              <div className="input-group">
+                <div className="input-box">
+                  <button type="submit">Donasi Sekarang</button>
+                </div>
+              </div>
         </form>
-        </div>
-    {/* </div> */}
-
-  </section>
+      </div>
+    </section>
     </body>
     </>
   );
 };
+
