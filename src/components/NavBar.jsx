b5aaa2f4d73398de88dc178a3ec091cb5c292ad9
Handayani Meiatara <91212078+Meiatara@users.noreply.github.com>
2023-06-16 00:19:42 +0700

    update navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,43 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./NavBar.css";
 
 function NavBar() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState('');
   const [click, setClick] = useState(false);
 
+  useEffect(() => {
+    const storedToken = localStorage.getItem('accessToken');
+    const storedUsername = localStorage.getItem('username');
+
+    if (storedToken && storedUsername) {
+      setIsLoggedIn(true);
+      setUsername(storedUsername);
+    }
+  }, [isLoggedIn, username]);
+
+  const handleLogout = () => {
+    localStorage.clear();
+    alert('Logout successfully');
+    navigate('/');
+    window.location.reload();
+  };
+
   const handleClick = () => setClick(!click);
+  
   return (
-    <>
+    <div>
       <nav className="navbar">
         <div className="nav-container">
-          <NavLink exact to="/" className="nav-logo">
-            {/* GreenPeace */}
+          <NavLink to="/" className="nav-logo">
             <img src="https://i.ibb.co/DgJ1wqm/logo-gp.png" alt="logo-gp" border="0"/>
-            {/* <i className="fas fa-code"></i> */}
           </NavLink>
 
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <NavLink
-                exact
                 to="/"
                 activeClassName="active"
                 className="nav-links"
@@ -30,7 +48,6 @@ function NavBar() {
             </li>
             <li className="nav-item">
               <NavLink
-                exact
                 to="/artikel"
                 activeClassName="active"
                 className="nav-links"
@@ -41,7 +58,6 @@ function NavBar() {
             </li>
             <li className="nav-item">
               <NavLink
-                exact
                 to="/program"
                 activeClassName="active"
                 className="nav-links"
@@ -52,7 +68,6 @@ function NavBar() {
             </li>
             <li className="nav-item">
               <NavLink
-                exact
                 to="/about"
                 activeClassName="active"
                 className="nav-links"
@@ -61,25 +76,42 @@ function NavBar() {
                 Tentang Kami
               </NavLink>
             </li>
-            <div className="nav-btn">
-              <NavLink to={"./donasi"}>
-              <button className="btn navbar-btn btn-primary" >
-                <span>DONASI</span>
-              </button>
-              </NavLink>
-              <NavLink to={"./login"}>
-                <button id="btn-login" className="btn navbar-btn btn-outline">
-                  <span>Login</span>
-                </button>
-              </NavLink>
-            </div>
+            {isLoggedIn ? (
+              <>
+              <div className="nav-btn">
+                <li>
+                  <span className="btn">Halo, {username}!</span>
+                </li>
+                <li>
+                  <button onClick={handleLogout} className="btn btn-outline">Logout</button>
+                </li>
+              </div>
+              
+              </>
+            ) : (
+              <>
+              <div className="nav-btn">
+                <NavLink to={"/donasi"}>
+                  <button className="btn navbar-btn btn-primary">
+                    <span>DONASI</span>
+                  </button>
+                </NavLink>
+                <NavLink to={"/login"}>
+                  <button id="btn-login" className="btn navbar-btn btn-outline">
+                    <span>Login</span>
+                  </button>
+                </NavLink>
+              </div>
+              </>
+            )}
           </ul>
+
           <div className="nav-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
           </div>
         </div>
       </nav>
-    </>
+    </div>
   );
 }
 
